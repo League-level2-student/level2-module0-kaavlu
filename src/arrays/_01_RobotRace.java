42af76c8f63dc1c6341edb8f56420ea00ad91dfa
kaavlu <42904778+kaavlu@users.noreply.github.com>
2021-07-09 16:59:20 -0700

    still working on this circle

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -3,116 +3,78 @@ import java.util.Random;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
+
+	Robot robot0 = new Robot();
+	Robot robot1 = new Robot();
+	Random random = new Random();
+	public 	double originX = 450;
+	public 	double originY = 300;
 	
+	public double angle = 0;
 	
-	public static void main(String[] args) {
-		
-		Robot[] robots = new Robot[1];
-		for(int i=0;i<robots.length; i++) {
-			robots[i] = new Robot();
-		}
-		
-		
-		int startingX = 450;
-		int startingY = 450;
-		double originX = 450;
-		double originY = 300;
-		double x = 0;
-		double y = 0;
-		int angle = 0;
-		y = startingY + Math.sin(angle)*150;
-		robots[0].setAngle(270);
-		robots[0].penDown();
-		robots[0].setRandomPenColor();
-		
-		robots[0].moveTo(startingX, startingY);
-		robots[0].setSpeed(1000);
-		int move = 1;
-		
-		while(robots[0].getY()>300) {
-			robots[0].move(move);
-			robots[0].turn(90);
-			robots[0].move(move);
-			robots[0].turn(-90);
-			move+=2;
-		}
-		robots[0].turn(90);
-		while(robots[0].getY()>150) {
-			robots[0].move(move);
-			robots[0].turn(90);
-			robots[0].move(move);
-			robots[0].turn(-90);
-			move-=2;
-		}
-		System.out.println(robots[0].getY());
-		robots[0].turn(90);
-		while(robots[0].getY()<300) {
-			robots[0].move(move);
-			robots[0].turn(90);
-			robots[0].move(move);
-			robots[0].turn(-90);
-			move+=2;
-		}
-		robots[0].turn(90);
-		while(robots[0].getY()<450) {
-			robots[0].move(move);
-			robots[0].turn(90);
-			robots[0].move(move);
-			robots[0].turn(-90);
-			move-=2;
-		}
-		robots[0].turn(90);
-		//robots[0].moveTo(startingX-200, startingY+200);
-		
-		
+	public int r = 150;
 	
-		
-		
-		
-		//robots[1].moveTo(300, 550);
+	public int x = (int) Math.round(originX + r * Math.sin(angle));
+	
+	public int y = (int) Math.round(originY + r * Math.cos(angle));
+	
+	public int rob0 = 1;
+	public int rob1 = 1;
+	public int rand0 = 0;
+	public int rand1 = 0;
+	
+	public void robot0(int random0) {
+		for(int i=0; i<random0; i++) {
 			
-		//5. use another for loop to iterate through the array and make each robot move 
-		//   a random amount less than 50.
-			/*
-			int rob = 0;
-		while(robots[0].getY()>0 && robots[1].getY()>0 && robots[2].getY()>0 && robots[3].getY()>0 && robots[4].getY()>0) {
+			angle = angle + Math.toRadians(5);
 			
-			for(int i=0; i<robots.length; i++) {				
-				Random random = new Random();
-				int randNum = random.nextInt(50);
-				robots[i].move(randNum);
-			    rob = i;
+			x = (int) Math.round(originX + r * Math.sin(angle));
+			
+			y = (int) Math.round(originY + r * Math.cos(angle));
+		
+			robot0.moveTo(x, y);
+			rob0++;
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
-		*/
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-		//x = startingX + Math.cos(angle)*150;
-		//y = startingY + Math.sin(angle)*150;
-		
-		//int xint = (int)x;
-		//int yint = (int)y;
-		
-		//robots[0].moveTo(xint, yint);
-		//angle++;
-			
-	//7. declare that robot the winner and throw it a party!
-			/*
-			if(rob==0) {
-				System.out.println("winner is 0");
-			}else if(rob==1) {
-				System.out.println("winner is 1");
-			}else if(rob==2) {
-				System.out.println("winner is 3");
-			}else if(rob==3) {
-				System.out.println("winner is 3");
-			}else {
-				System.out.println("winner is 4");
-			}
-			*/
-	//8. try different races with different amounts of robots.
-			
-	//9. make the robots race around a circular track.
-			
 	}
+	public void robot1(int random1) {
+		for(int i=0; i<random1; i++) {
+			
+			angle = angle + Math.toRadians(5);
+			
+			x = (int) Math.round(originX + r * Math.sin(angle));
+			
+			y = (int) Math.round(originY + r * Math.cos(angle));
+			
+			robot1.moveTo(x, y);
+			rob1++;
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void combo() {
+		while(rob0!=72 && rob1!=72) {
+			rand0 = random.nextInt(10);
+			rand1 = random.nextInt(10);
+			robot0(rand0);
+			robot1(rand1);
+			
+		}
+	}
+	public void run() {
+		combo();
+	}
+				
 }
 
